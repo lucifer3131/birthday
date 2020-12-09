1d0c543055f638e987778abe138344b3ea51840a
lucifer3131 <75742843+lucifer3131@users.noreply.github.com>
2020-12-09 21:49:37 +0530

    Add files via upload

---
+++ birth/script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "karthi" && b=="2003-12-10")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
